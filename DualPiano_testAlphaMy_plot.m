6ef63ad59baa0cb9cb4ef31997d2d56956df62f6
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-04-03 14:16:40 +0200

    Fehler in DualPiano_testAlphaMy_preproc behoben Funktion DualPiano_testAlphaMy_plot(…) erzeugt jetzt auch ein PDF mit übergeordneter Überschrift. Skript DualPiano_testAlphaMy_preproc und Funktion DualPiano_testAlphaMy_plot(…) ausführlich dokumentiert.

--- DualPiano_testAlphaMy_plot.m
+++ DualPiano_testAlphaMy_plot.m
@@ -1,6 +1,21 @@
 function DualPiano_testAlphaMy_plot( time_data, freq_data, component, ...
   varargin )
-% DUALPIANO_TESTALPHAMY_PLOT
+% DUALPIANO_TESTALPHAMY_PLOT generates a graphic consisting of multiple
+% panels including different representations of a choosen component.
+%
+% Params:
+%   time_data     NxM matrix with N components of a length of M timepoints
+%   freq_data     fieldtrip data structure including freq data
+%   component     number of the specific component
+%   
+% Varargin:
+%   timeOffset    Offset for the time axis of the time response
+%
+% This function requires the fieldtrip toolbox
+%
+% See also DUALPIANO_FFTCOMMON, DUALPIANO_SINGLEPLOTTFR
+
+% Copyright (C) 2017, Daniel Matthes, MPI CBS
 
 % -------------------------------------------------------------------------
 % Check input
@@ -12,11 +27,62 @@ switch length(varargin)
     timeOffset    = varargin{1};
 end
 
+% -------------------------------------------------------------------------
+% Create panels for headline and the two different subplots
+% -------------------------------------------------------------------------
 f = figure(5);
-p = uipanel('Parent', f, 'Position', [0 0 0.5 1]);
-q = uipanel('Parent', f, 'Position', [0.5 0 0.5 1]);
-DualPiano_fftCommon(time_data(component, :), 0, p, timeOffset);
-DualPiano_singleplotTFR(freq_data, 'all', component, [1 30], q);
+p = uipanel('Parent', f, 'BackgroundColor', 'white', 'BorderType', ...      % create panel p for the time response and the frequency response
+  'none', 'Position', [0 0 0.5 0.95]);
+q = uipanel('Parent', f, 'BackgroundColor', 'white', 'BorderType', ...      % create panel q for the time-frequency response 
+  'none', 'Position', [0.5 0 0.5 0.95]);
+r = uipanel('Parent', f, 'BackgroundColor', 'white', 'BorderType', ...      % create panel r for the headline
+  'none', 'Position', [0 0.95 1 0.05]);
+
+% -------------------------------------------------------------------------
+% Build headline of the figure
+% -------------------------------------------------------------------------
+dyad = freq_data.Mat_cond_pair(1,2);
+
+if (component == 2 || component == 3)
+  player = 1;
+elseif (component == 5 || component == 6)
+  player = 2;
+end
+
+switch component
+  case 2
+    comp_text = 'motor right';
+  case 3
+    comp_text = 'motor left';
+  case 5
+    comp_text = 'motor right';
+  case 6
+    comp_text = 'motor left';
+end
+
+fig_title = sprintf('Dyad: %d, Player: %d, Component: %s', dyad, player,...
+  comp_text);
+r.Title = fig_title; 
+r.TitlePosition = 'centerbottom'; 
+r.FontSize = 12;
+r.FontWeight = 'bold';
+
+% -------------------------------------------------------------------------
+% Plot FFT and TFR
+% -------------------------------------------------------------------------
+DualPiano_fftCommon(time_data(component, :), 0, p, timeOffset);             % build a 2x1 subplot with time and frequency response
+DualPiano_singleplotTFR(freq_data, 'all', component, [1 30], q);            % plot time-frequency response
+
+% -------------------------------------------------------------------------
+% Save graphic as pdf-File
+% -------------------------------------------------------------------------
+h=gcf;
+set(h, 'PaperOrientation','landscape');
+set(h, 'PaperType','a3');
+set(h, 'PaperUnit', 'centimeters');
+set(h, 'PaperSize', [42 29.7]);
+set(h, 'unit', 'normalized', 'Position', [0 0 0.9 0.9]);
+print(gcf, '-dpdf', '../../results/DualPiano_analysis/output.pdf');
 
 end
 
--- DualPiano_testAlphaMy_preproc.m
+++ DualPiano_testAlphaMy_preproc.m
@@ -1,3 +1,19 @@
+% DUALPIANO_TESTALPHAMY_PREPROC
+%
+% This script preprocesses the whole data of one dyad from the "dual piano"
+% study, so that various components or rather parts of various components
+% could be visually analysied afterwards by using FFT and TFR plots.
+%
+% This script requires the fieldtrip toolbox
+%
+% See also DUALPIANO_MEANOVERTRIALS, DUALPIANO_EXTRACTSIGSEG,
+% DUALPIANO_FREQANALYSIS
+
+% Copyright (C) 2017, Daniel Matthes, MPI CBS
+
+% -------------------------------------------------------------------------
+% This part applies a bandpass to all trials in every condition
+% -------------------------------------------------------------------------
 % warning('off','all'); 
 % 
 % data_CF = DualPiano_bandpass(data_CF, 1, 30, false);
@@ -7,17 +23,38 @@
 % 
 % warning('on','all'); 
 
+% -------------------------------------------------------------------------
+% In this part the mean over all trials for every condition is calculated
+% -------------------------------------------------------------------------
 data_CF_av = DualPiano_meanOverTrials(data_CF);
 data_CU_av = DualPiano_meanOverTrials(data_CU);
 data_UF_av = DualPiano_meanOverTrials(data_UF);
 data_UU_av = DualPiano_meanOverTrials(data_UU);
 
+% -------------------------------------------------------------------------
+% In this part the averaged trials are trimmed
+% 1: phrase 1 - second 0 to 4
+% 2: pause - second 4 to 8
+% 3: phrase 2 - second 8 to 11
+% -------------------------------------------------------------------------
 data_CF_avSeg = DualPiano_extractSigSeg(data_CF_av, 2);
 data_CU_avSeg = DualPiano_extractSigSeg(data_CU_av, 2);
 data_UF_avSeg = DualPiano_extractSigSeg(data_UF_av, 2);
 data_UU_avSeg = DualPiano_extractSigSeg(data_UU_av, 2);
 
+% -------------------------------------------------------------------------
+% This part calculates time-frequency data for every component in every
+% condition averaged over all trials
+% -------------------------------------------------------------------------
 data_CF_freq = DualPiano_freqanalysis(data_CF);
 data_CU_freq = DualPiano_freqanalysis(data_CU);
 data_UF_freq = DualPiano_freqanalysis(data_UF);
 data_UU_freq = DualPiano_freqanalysis(data_UU);
+
+% -------------------------------------------------------------------------
+% Propagation of the Mat_cond_pair matrix
+% -------------------------------------------------------------------------
+data_CF_freq.Mat_cond_pair = data_CF.Mat_cond_pair;
+data_CU_freq.Mat_cond_pair = data_CU.Mat_cond_pair;
+data_UF_freq.Mat_cond_pair = data_UF.Mat_cond_pair;
+data_UU_freq.Mat_cond_pair = data_UU.Mat_cond_pair;
